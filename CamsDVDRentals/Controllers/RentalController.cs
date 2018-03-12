acae0a9597a48de500cd512ea077499518a9272c
unknown <cambfilm@gmail.com>
2018-03-12 11:06:34 -0400

    added rental, home controllers, home view

---
+++ CamsDVDRentals/Controllers/RentalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CamsDVDRentals.Controllers
+{
+    public class RentalController : Controller
+    {
+        // GET: Rental
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
